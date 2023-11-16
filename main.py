641590d05c1be32dc43ea4c8d98551c9eb944cb2
Rickard Lindberg <rickard@rickardlindberg.me>
2023-11-16 07:47:25 +0100

    Examples should debug.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 if __name__ == "__main__":
     import operator
     run_simulation(
-        [
+        actors=[
             ArgsParser(),
             Greeter(),
             Factorial(),
         ],
-        {
+        extra={
             "print": lambda text: print(f"Print: {text}\n"),
             "sub": operator.sub,
             "mul": operator.mul,
-        }
+        },
+        debug=True
     )
