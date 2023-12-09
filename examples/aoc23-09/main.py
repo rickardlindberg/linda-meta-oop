50ce87cea13b9382906197913fcad243d23a4661
Rickard Lindberg <rickard@rickardlindberg.me>
2023-12-09 14:11:54 +0100

    Advet of Code 2023, Day 9, Part 1.

---
+++ examples/aoc23-09/main.py
@@ -0,0 +1,34 @@
+import operator
+
+def run(actors, message):
+    return run_simulation(
+        actors=actors,
+        extra={
+            "add": operator.add,
+            "sub": operator.sub,
+            "mul": operator.mul,
+            "sum": sum,
+            "max": max,
+            "min": min,
+            "dict": dict,
+        },
+        debug=True,
+        fail=False,
+        messages=[message]
+    )
+
+def run_gives(actors, message, expected):
+    actual = run(actors, message)
+    assert actual == expected, f"{actual} == {expected}"
+
+if __name__ == "__main__":
+    run_gives(
+        [CLI(), Parser(), Differ(), Summer()],
+        ["Args", "example.txt"],
+        [["Result", 114]]
+    )
+    run_gives(
+        [CLI(), Parser(), Differ(), Summer()],
+        ["Args", "input.txt"],
+        [["Result", 1916822650]]
+    )
