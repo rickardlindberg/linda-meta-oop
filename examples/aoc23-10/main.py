3f11645491562799b61b44b1fea9106447d2a73e
Rickard Lindberg <rickard@rickardlindberg.me>
2023-12-10 11:26:18 +0100

    Advet of Code 2023, Day 10, Part 1.

---
+++ examples/aoc23-10/main.py
@@ -0,0 +1,58 @@
+import operator
+
+def run(actors, message):
+    return run_simulation(
+        actors=actors,
+        extra={
+            "add": operator.add,
+            "sub": operator.sub,
+            "mul": operator.mul,
+            "sum": sum,
+            "max": max,
+            "min": min,
+            "dict": dict,
+        },
+        debug=True,
+        fail=False,
+        messages=[message]
+    )
+
+def run_gives(actors, message, expected):
+    actual = run(actors, message)
+    assert actual == expected, f"{actual} == {expected}"
+
+if __name__ == "__main__":
+    run_gives(
+        [Parser()],
+        ["File", "S7\nLJ\n"],
+        [['Map',
+            ['Node', 0, 0, "START"],
+            ['Node', 1, 0, [0, 0], [1, 1]],
+            ['Node', 0, 1, [0, 0], [1, 1]],
+            ['Node', 1, 1, [1, 0], [0, 1]]]]
+    )
+    run_gives(
+        [Flattener()],
+        ['Map',
+            ['Node', 0, 0, "START"],
+            ['Node', 1, 0, [0, 0], [1, 1]],
+            ['Node', 0, 1, [0, 0], [1, 1]],
+            ['Node', 1, 1, [1, 0], [0, 1]]],
+        [['FlatMap',
+            "START",
+            [0, 3],
+            [0, 3],
+            [1, 2]]]
+    )
+    run_gives(
+        [Flattener()],
+        ['Map',
+            ['Node', 0, 0]],
+        [['FlatMap',
+            []]]
+    )
+    run_gives(
+        [CLI(), Parser(), Flattener(), StepMaximizer()],
+        ["Args", "input.txt"],
+        [['Result', 6786]]
+    )
