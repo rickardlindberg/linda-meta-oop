c0d3a301e264e4db90966bbc0faffa1863a042ba
Rickard Lindberg <rickard@rickardlindberg.me>
2023-12-12 17:24:49 +0100

    Advet of Code 2023, Day 12, Part 2.

--- examples/aoc23-12/main.py
+++ examples/aoc23-12/main.py
@@ -24,12 +24,27 @@ def run_gives(actors, message, expected):
 
 if __name__ == "__main__":
     run_gives(
-        [CLI(), Parser(), CountArrangements()],
+        [CLI(), Parser(), CountArrangements("Records")],
         ["Args", "example.txt"],
         [['PlacementCounts', 1, 4, 1, 1, 4, 10]]
     )
     run_gives(
-        [CLI(), Parser(), CountArrangements(), Summer()],
+        [CLI(), Parser(), CountArrangements("Records"), Summer()],
         ["Args", "input.txt"],
         [['Result', 7344]]
+    )
+    run_gives(
+        [Unfolder()],
+        ["Records", ["Record", ".#", [1]]],
+        [["Unfolded", ["Record", ".#?.#?.#?.#?.#", [1,1,1,1,1]]]]
+    )
+    run_gives(
+        [CLI(), Parser(), Unfolder(), CountArrangements("Unfolded"), Summer()],
+        ["Args", "example.txt"],
+        [['Result', 525152]]
+    )
+    run_gives(
+        [CLI(), Parser(), Unfolder(), CountArrangements("Unfolded"), Summer()],
+        ["Args", "input.txt"],
+        [['Result', 1088006519007]]
     )
