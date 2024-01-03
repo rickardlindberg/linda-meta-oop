2a672db9dbb2663a628bf108d949ca783f57c865
Rickard Lindberg <rickard@rickardlindberg.me>
2024-01-03 08:28:54 +0100

    Convert tests to examples and generate main.

--- examples/aoc23-01/make.sh
+++ examples/aoc23-01/make.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-python ../../rlmeta/rlmeta.py --support --compile puzzle.rlmeta --copy main.py > puzzle.py
+python ../../rlmeta/rlmeta.py --support --compile puzzle.rlmeta --main > puzzle.py
 
-python puzzle.py "$@"
+python puzzle.py
